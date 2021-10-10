4409dabd37f0e50431f88398c48cbb7e9e61cf14
MOHIT VAISH <mohitvaish24@gmail.com>
2021-10-11 00:57:51 +0530

    cron interval added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $file = 'command1_output.log';
-        $schedule->command('matchlist:update')->everyMinute()->sendOutputTo($file);
+        
+        $schedule->command('matchlist:update')->everyFifteenMinutes();
     }
 
     /**
